a88ad20599b1dea1ca4ba5688443095ec5c07f1d
Bruno Leonardo <bruno@MacBook-Air-de-Bruno.local>
2021-09-28 00:16:14 -0300

    pagina home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,21 @@
 import Head from 'next/head';
 
+import styles from './home.module.scss';
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>Hello Next</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world</h1>
+          <p>Get access to all the publications for <span>$9.90 mounth</span></p>
+        </section>
+        <img src="/images/avatar.svg" alt="" />
+      </main>
     </>
   )
 }
